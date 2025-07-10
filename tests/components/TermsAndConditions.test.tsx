afbda317be4fca1d25748ecfd6ef1844f3ba5139
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-07-10 10:46:54 -0700

    test terms & condition for default

---
+++ tests/components/TermsAndConditions.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import TermsAndConditions from "../../src/components/TermsAndConditions";
+
+describe("TermsAndConditions", () => {
+  it("should return text and  unchecked and inactive btn", () => {
+    render(<TermsAndConditions />);
+
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent(/Terms & Conditions/i);
+
+    const checkbox = screen.getByRole("checkbox");
+    expect(checkbox).toBeInTheDocument();
+    expect(checkbox).not.toBeChecked();
+
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/submit/i);
+    expect(button).toBeDisabled();
+  });
+});
