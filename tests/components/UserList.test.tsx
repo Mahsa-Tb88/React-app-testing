eb3bc4c07c8be1f412e12f0788bfb0f750f1e385
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-07-10 09:31:32 -0700

    test userList

---
+++ tests/components/UserList.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import UserList from "../../src/components/UserList";
+import { User } from "../../src/entities";
+import { link } from "fs";
+
+describe("UserList", () => {
+  it("should return no users when user list is empty", () => {
+    render(<UserList users={[]} />);
+    expect(screen.getByText(/no user/i)).toBeInTheDocument();
+  });
+
+  it("should return user list when user list is not empty", () => {
+    const users: User[] = [
+      { id: 1, name: "Mahsa" },
+      { id: 2, name: "Hossein" },
+    ];
+    render(<UserList users={users} />);
+    users.forEach((user) => {
+      const link = screen.getByRole("link", { name: user.name });
+      expect(link).toBeInTheDocument();
+      expect(link).toHaveAttribute("href", `/users/${user.id}`);
+    });
+  });
+});
