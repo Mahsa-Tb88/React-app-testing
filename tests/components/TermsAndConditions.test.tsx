c92da965a8e3ac72ff589cb16d932bbe820309b4
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-07-10 10:57:10 -0700

    add userevent for interaction - done test ters and condition

--- tests/components/TermsAndConditions.test.tsx
+++ tests/components/TermsAndConditions.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import TermsAndConditions from "../../src/components/TermsAndConditions";
+import userEvent from "@testing-library/user-event";
 
 describe("TermsAndConditions", () => {
   it("should return text and  unchecked and inactive btn", () => {
@@ -17,5 +18,16 @@ describe("TermsAndConditions", () => {
     expect(button).toBeInTheDocument();
     expect(button).toHaveTextContent(/submit/i);
     expect(button).toBeDisabled();
+  });
+
+  it("should return enable button when checkbox is checked", async () => {
+    render(<TermsAndConditions />);
+
+    const checkbox = screen.getByRole("checkbox");
+    const user = userEvent.setup();
+    await user.click(checkbox);
+
+    const button = screen.getByRole("button");
+    expect(button).toBeEnabled();
   });
 });
