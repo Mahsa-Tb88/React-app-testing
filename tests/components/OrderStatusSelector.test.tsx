209092e340f2662b5256fb43e394fc53dd95edd5
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-07-14 09:22:52 -0700

    improve test

--- tests/components/OrderStatusSelector.test.tsx
+++ tests/components/OrderStatusSelector.test.tsx
@@ -2,13 +2,37 @@ import { render, screen } from "@testing-library/react";
 
 import OrderStatusSelector from "../../src/components/OrderStatusSelector";
 import { Theme } from "@radix-ui/themes";
+import userEvent from "@testing-library/user-event";
 
 describe("OrderStatusSelector", () => {
-  it("should return selector with default value new", () => {
+  const renderComponent = () => {
     render(
       <Theme>
         <OrderStatusSelector onChange={vi.fn()} />
       </Theme>
     );
+
+    return {
+      trigger: screen.getByRole("combobox"),
+      getOptions: () => screen.findAllByRole("option"),
+    };
+  };
+
+  it("should return new as a default value", () => {
+    const { trigger } = renderComponent();
+    expect(trigger).toHaveTextContent("New");
+  });
+
+  it("should return correct status", async () => {
+    const { trigger, getOptions } = renderComponent();
+
+    const user = userEvent.setup();
+    await user.click(trigger);
+
+    const options = await getOptions();
+    expect(options).toHaveLength(3);
+
+    const lables = options.map((option) => option.textContent);
+    expect(lables).toEqual(["New", "Processed", "Fulfilled"]);
   });
 });
