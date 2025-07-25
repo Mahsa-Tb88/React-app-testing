12e954a20a339ed34173513ef1d1090a5beb14d7
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-07-25 08:34:15 -0700

    usinf MSw-  product database

--- tests/components/ProductList.test.tsx
+++ tests/components/ProductList.test.tsx
@@ -2,8 +2,20 @@ import { render, screen } from "@testing-library/react";
 import ProductList from "../../src/components/ProductList";
 import { http, HttpResponse } from "msw";
 import { server } from "../mocks/server";
+import { db } from "../mocks/db";
 
 describe("Product List", () => {
+  const productsId: number[] = [];
+  beforeAll(() => {
+    [1, 2, 3].forEach(() => {
+      const product = db.product.create();
+      productsId.push(product.id);
+    });
+  });
+
+  afterAll(() => {
+    db.product.deleteMany({ where: { id: { in: productsId } } });
+  });
   it("should render the list of products", async () => {
     render(<ProductList />);
     const items = await screen.findAllByRole("listitem");
