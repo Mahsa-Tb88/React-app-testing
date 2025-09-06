4ff40058997ce6697730a4edd877cfc89a7983bc
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-09-05 18:49:42 -0700

    test label es

--- tests/components/Label.test.tsx
+++ tests/components/Label.test.tsx
@@ -3,16 +3,33 @@ import Label from "../../src/components/Label";
 import { LanguageProvider } from "../../src/providers/language/LanguageProvider";
 
 describe("Label", () => {
-  it.each([
-    { labelId: "welcome", text: "Welcome" },
-    { labelId: "new_product", text: "New Product" },
-    { labelId: "edit_product", text: "Edit Product" },
-  ])("should render ${text} for labelId", ({ labelId, text }) => {
-    render(
-      <LanguageProvider language="en">
-        <Label labelId={labelId} />
-      </LanguageProvider>
-    );
-    expect(screen.getByText(text)).toBeInTheDocument();
+  describe("give the current lamguage is En", () => {
+    it.each([
+      { labelId: "welcome", text: "Welcome" },
+      { labelId: "new_product", text: "New Product" },
+      { labelId: "edit_product", text: "Edit Product" },
+    ])("should render ${text} for labelId", ({ labelId, text }) => {
+      render(
+        <LanguageProvider language="en">
+          <Label labelId={labelId} />
+        </LanguageProvider>
+      );
+      expect(screen.getByText(text)).toBeInTheDocument();
+    });
+  });
+
+  describe("give the current lamguage is ES", () => {
+    it.each([
+      { labelId: "welcome", text: "Bienvenidos" },
+      { labelId: "new_product", text: "Nuevo Producto" },
+      { labelId: "edit_product", text: "Editar Producto" },
+    ])("should render ${text} for labelId", ({ labelId, text }) => {
+      render(
+        <LanguageProvider language="es">
+          <Label labelId={labelId} />
+        </LanguageProvider>
+      );
+      expect(screen.getByText(text)).toBeInTheDocument();
+    });
   });
 });
