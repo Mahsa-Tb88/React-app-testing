253ae4b058c7b0bb9c28dd099fde7085246c33d1
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-09-12 11:00:51 -0700

    refactoring

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -15,10 +15,18 @@ describe("productFrom", () => {
     db.category.delete({ where: { id: { equals: category.id } } });
   });
 
-  it("should render form", async () => {
-    render(<ProductForm onSubmit={vi.fn()} />, { wrapper: AllProvider });
+  const renderComponent = (product?: Product) => {
+    render(<ProductForm product={product} onSubmit={vi.fn()} />, { wrapper: AllProvider });
 
-    await screen.findByRole("form");
+    return {
+      waitForFormToLoad: () => screen.findByRole("form"),
+    };
+  };
+
+  it("should render form", async () => {
+    const { waitForFormToLoad } = renderComponent();
+
+    await waitForFormToLoad();
     expect(screen.getByPlaceholderText(/name/i)).toBeInTheDocument();
     expect(screen.getByPlaceholderText(/price/i)).toBeInTheDocument();
     expect(screen.getByRole("combobox", { name: /category/i })).toBeInTheDocument();
@@ -31,8 +39,8 @@ describe("productFrom", () => {
       price: 10,
       categoryId: category.id,
     };
-    render(<ProductForm product={product} onSubmit={vi.fn()} />, { wrapper: AllProvider });
-    await screen.findByRole("form");
+    const { waitForFormToLoad } = renderComponent(product);
+    await waitForFormToLoad();
     expect(screen.getByPlaceholderText(/name/i)).toHaveValue(product.name);
     expect(screen.getByPlaceholderText(/price/i)).toHaveValue(product.price.toString());
     expect(screen.getByRole("combobox", { name: /category/i })).toHaveTextContent(category.name);
